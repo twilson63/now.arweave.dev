cc5eea8a6595d68a05d047680401b1bd8dc4f93e
Tom Wilson <tom@hyper.io>
2022-08-20 12:12:21 +0000

    updated meme exchange to new stamp coin test contract

--- src/lib/app.js
+++ src/lib/app.js
@@ -11,7 +11,7 @@ const arweave = Arweave.init({
 const { WarpWebFactory, LoggerFactory } = window.warp;
 LoggerFactory.INST.logLevel("error");
 
-const STAMPCOIN = "9nDWI3eHrMQbrfs9j8_YPfLbYJmBodgn7cBCG8bii4o";
+const STAMPCOIN = "aSMILD7cEJr93i7TAVzzMjtci_sGkXcWnqpDkG6UGcA";
 const warp = WarpWebFactory.memCached(arweave);
 
 export const listAssets = () => Market.listAssets(STAMPCOIN).runWith({ warp, arweave }).toPromise()

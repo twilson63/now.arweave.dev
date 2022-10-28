7cdd909f092c8b04b5727d2a84c99e708a792bb3
Tom Wilson <tom@hyper.io>
2022-10-28 14:05:52 +0000

    added content dialog

--- src/components/navbar.svelte
+++ src/components/navbar.svelte
@@ -131,29 +131,28 @@
   </div>
   <div class="flex-none">
     <ul class="menu menu-horizontal p-0 items-center space-x-none">
-      {#if profile.owner}
-        <li on:click={post}>
-          <div
-            class="tooltip tooltip-bottom tooltip-primary"
-            data-tip="POST Asset to Now"
+      <li on:click={post}>
+        <div
+          class="tooltip tooltip-bottom tooltip-primary"
+          data-tip="POST Asset to Now"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="w-6 h-6"
           >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke-width="1.5"
-              stroke="currentColor"
-              class="w-6 h-6"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5m-13.5-9L12 3m0 0l4.5 4.5M12 3v13.5"
-              />
-            </svg>
-          </div>
-        </li>
-      {/if}
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5m-13.5-9L12 3m0 0l4.5 4.5M12 3v13.5"
+            />
+          </svg>
+        </div>
+      </li>
+
       <li on:click={() => about()}>
         <div
           class="tooltip tooltip-bottom tooltip-primary"

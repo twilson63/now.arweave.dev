19ad844369f9c4550278744e443b38eb31a8bef9
mogulx_operates <mogulx_operates@yahoo.com>
2023-02-10 16:21:38 -0600

    feat: get host

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,7 +1,19 @@
-import { BigNumber } from 'bignumber.js'
+import { BigNumber } from "bignumber.js";
 
-export const barToAtomic = (bar) => BigNumber.clone({ DECIMAL_PLACES: 6 })(bar).shiftedBy(6).toFixed(6)
-export const atomicToBar = (atomic) => BigNumber.clone({ DECIMAL_PLACES: 6 })(atomic).shiftedBy(-6).toFixed(6)
+export const barToAtomic = (bar) =>
+  BigNumber.clone({ DECIMAL_PLACES: 6 })(bar).shiftedBy(6).toFixed(6);
+export const atomicToBar = (atomic) =>
+  BigNumber.clone({ DECIMAL_PLACES: 6 })(atomic).shiftedBy(-6).toFixed(6);
 
-export const stampToAtomic = (stamp) => BigNumber.clone({ DECIMAL_PLACES: 12 })(stamp).shiftedBy(12).toFixed(0)
-export const atomicToStamp = (atomic) => BigNumber.clone({ DECIMAL_PLACES: 12 })(atomic).shiftedBy(-12).toFixed(12)
+export const stampToAtomic = (stamp) =>
+  BigNumber.clone({ DECIMAL_PLACES: 12 })(stamp).shiftedBy(12).toFixed(0);
+export const atomicToStamp = (atomic) =>
+  BigNumber.clone({ DECIMAL_PLACES: 12 })(atomic).shiftedBy(-12).toFixed(12);
+
+export const getHost = (location) => {
+  const parts = location?.hostname?.split(".");
+  parts.shift();
+  const host = parts?.join(".");
+  if (host === "localhost") return "arweave.dev";
+  return host || "arweave.dev";
+};
